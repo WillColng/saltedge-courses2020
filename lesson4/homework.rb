413a5446154bc2d1c365e64a02dd8be84741e2b0
igrwrdn <igorworden@gmail.com>
2020-02-25 16:05:01 +0200

    Homework4

---
+++ lesson4/homework.rb
@@ -0,0 +1,27 @@
+a=-123
+puts a.odd?
+
+b=6893
+puts b.next
+
+f=0.pred
+puts f
+
+c=12.to_f/4.5
+puts c.round
+
+d=28%6*4.7
+e=d.floor
+puts e
+
+puts "Brand new bicycle\r\n".delete "\r\n"
+
+puts "Brand new bicycle"[6..8]
+
+puts "Brand new bicycle".end_with?("bicycle")
+
+puts "Brand new bicycle".index('w')
+
+p"I thought I knew everything before that night".partition("everything")
+
+puts "Final result from Float/Integer part is: "+(b+f+c.round+e).to_s
